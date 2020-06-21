2299588b57cb8cbf30299051d3c743363b4aacf2
drsherlock <zain.delhi27@hotmail.com>
2020-06-22 01:53:09 +0530

    Update socket handling

--- inquizit/controllers/ws_controller.py
+++ inquizit/controllers/ws_controller.py
@@ -1,15 +1,15 @@
 from aiohttp import web, WSMsgType
 from bson.objectid import ObjectId
 
-from models import Game
+from models import Room
 
 routes = web.RouteTableDef()
 
 
-@routes.get('/ws/{game_id}/{player_id}')
+@routes.get('/ws/{room_id}/{user_id}')
 async def create(request):
-    game_id = request.match_info['game_id']
-    player_id = request.match_info['player_id']
+    room_id = request.match_info['room_id']
+    user_id = request.match_info['user_id']
 
     ws_current = web.WebSocketResponse()
     ws_ready = ws_current.can_prepare(request)
@@ -19,54 +19,53 @@ async def create(request):
 
     await ws_current.prepare(request)
 
-    query = {'_id': ObjectId(game_id), 'active': True}
-    game = await Game.find_game(query=query, db=request.app['mongodb'])
+    query = {'_id': ObjectId(room_id), 'active': True}
+    room = await Room.find_room(query=query, db=request.app['mongodb'])
 
-    if game is None:
-        msg = "Game over, start a new game"
-        await ws_current.send_json({'action': 'error', 'name': player_id, 'msg': game_id})
+    if room is None:
+        msg = "Room is closed, create a new room"
+        await ws_current.send_json({'action': 'error', 'name': user_id, 'msg': room_id})
         await ws_current.close()
         return
 
-    if not any(player_id == str(player['_id']) for player in game['players']):
-        msg = "You have not joined the game"
-        await ws_current.send_json({'action': 'error', 'name': player_id, 'msg': msg})
+    if not any(user_id == str(user['_id']) for user in room['users']):
+        msg = "You have not joined the room"
+        await ws_current.send_json({'action': 'error', 'name': user_id, 'msg': msg})
         await ws_current.close()
         return
 
-    await ws_current.send_json({'action': 'connect', 'name': player_id})
+    await ws_current.send_json({'action': 'connect', 'name': user_id})
 
-    for player in game['players']:
-        if str(player['_id']) in request.app['websockets']:
-            player_ws = request.app['websockets'][str(player['_id'])]
-            await player_ws.send_json({'action': 'join', 'name': player_id})
+    for user in room['users']:
+        if str(user['_id']) in request.app['websockets']:
+            user_ws = request.app['websockets'][str(user['_id'])]
+            await user_ws.send_json({'action': 'join', 'name': user_id})
 
-    request.app['websockets'][player_id] = ws_current
+    request.app['websockets'][user_id] = ws_current
 
     async for msg in ws_current:
         if msg.type == WSMsgType.text:
             if msg.data == 'close':
                 await ws_current.close()
             else:
-                for player in game['players']:
-                    if str(player['_id']) in request.app['websockets']:
-                        player_ws = request.app['websockets'][str(
-                            player['_id'])]
-                        if player_ws is not ws_current:
-                            await player_ws.send_json(
-                                {'action': 'sent', 'name': player_id, 'text': msg.data})
+                for user in room['users']:
+                    if str(user['_id']) in request.app['websockets']:
+                        user_ws = request.app['websockets'][str(user['_id'])]
+                        if user_ws is not ws_current:
+                            await user_ws.send_json(
+                                {'action': 'sent', 'name': user_id, 'text': msg.data})
         elif msg.type == WSMsgType.ERROR:
             print('ws connection closed with exception %s' %
                   ws_current.exception())
         else:
             break
 
-    if player_id in request.app['websockets']:
-        del request.app['websockets'][player_id]
+    if user_id in request.app['websockets']:
+        del request.app['websockets'][user_id]
 
-    for player in game['players']:
-        if str(player['_id']) in request.app['websockets']:
-            player_ws = request.app['websockets'][str(player['_id'])]
-            await player_ws.send_json({'action': 'disconnect', 'name': player_id})
+    for user in room['users']:
+        if str(user['_id']) in request.app['websockets']:
+            user_ws = request.app['websockets'][str(user['_id'])]
+            await user_ws.send_json({'action': 'disconnect', 'name': user_id})
 
     return ws_current
--- inquizit/helpers/user_helper.py
+++ inquizit/helpers/user_helper.py
@@ -1,4 +1,6 @@
-from models import User
+from bson.objectid import ObjectId
+
+from models import User, Room
 
 
 async def find_user(request):
@@ -28,3 +30,41 @@ async def create_user(request):
         user_id = user['_id']
 
     return {'user_id': str(user_id)}
+
+
+async def verify_user(request):
+    user_id = request.user_id
+    if user_id == "":
+        msg = "Token is invalid"
+        return {'error': msg}
+
+    room_id = request.match_info['room_id']
+
+    query = {'_id': ObjectId(user_id)}
+    user = await User.find_user(
+        query=query, db=request.app['mongodb'])
+    if user is None:
+        msg = "User does not exist"
+        return {'error': msg}
+
+    query = {'_id': ObjectId(room_id), 'active': True}
+    room = await Room.find_room(query=query, db=request.app['mongodb'])
+    if room is None:
+        msg = "Room does not exist"
+        return {'error': msg}
+
+    room = await check_user_in_room(user_id, request)
+    if room_id != str(room['_id']):
+        return {'roomId': str(room['_id']), 'inRoom': True}
+
+    return {'userId': str(user['_id']), 'username': user['username']}
+
+
+async def check_user_in_room(user_id, request):
+    query = {'users': {'$elemMatch': {
+        '_id': ObjectId(user_id)}}, 'active': True}
+    room = await Room.find_user_in_room(query=query,
+                                        db=request.app['mongodb'])
+    if room is None:
+        return False
+    return room
